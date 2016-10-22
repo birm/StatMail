0f9f53afd7de8b716fe97b765df955fc4731ffc7
Birm <birm@rbirm.us>
2016-10-21 21:52:56 -0400

    add remote_run ssh and scan log

--- statmail/Scan.py
+++ statmail/Scan.py
@@ -1,6 +1,10 @@
 from .SMBase import SMBase
 from .Types import Types
 
+import subprocess
+import sys
+import logging
+
 """ A class to keep track of server scans.
 This is so that they can be collected into a list and re-run or
 queried after the fact.
@@ -19,7 +23,16 @@ class Scan(SMBase):
 
     def remote_run(self, cmd):
         """Run a command on the host for the class."""
-        pass
+        ssh = subprocess.Popen(["ssh", "%s" % self.host, cmd],shell=False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        result = ssh.stdout.readlines()
+        if result == []:
+            error = ssh.stderr.readlines()
+            logging.warning("[ssh err: " + self.host +"]" + error)
+            return "ssh-err"
+        else:
+            return join(result)
 
     def scan(self):
         """Run the scan."""
